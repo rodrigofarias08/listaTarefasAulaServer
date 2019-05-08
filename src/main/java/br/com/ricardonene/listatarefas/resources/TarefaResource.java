5a44f5b127e0835cb97e36fce2fd9984ed4ddcc5
Rodrigo Farias <rodrigofarias08@gmail.com>
2019-05-07 22:15:55 -0300

    Criado projeto pelo professor para usar em aula.

--- src/main/java/br/com/ricardonene/listatarefas/resources/TarefaResource.java
+++ src/main/java/br/com/ricardonene/listatarefas/resources/TarefaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ public class TarefaResource {
 	@Autowired
 	private TarefaService service;
 	
+	@CrossOrigin
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<Tarefa>> findAll() {
 		List<Tarefa> lista =service.findAll(); 
